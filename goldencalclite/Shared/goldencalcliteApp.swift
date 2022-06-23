e444e6f7609e19de9ada8b9a4b71589b20876742
Olexander <kvazis@gmail.com>
2022-06-24 00:48:57 +0300

    goldencalc

---
+++ goldencalclite/Shared/goldencalcliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  goldencalcliteApp.swift
+//  Shared
+//
+//  Created by Oleksandr Koreniuk on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct goldencalcliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
