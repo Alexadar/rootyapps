2dbd2eac37dfc221e0f570a7c1e6d345aabf89fe
Olexander <kvazis@gmail.com>
2025-09-20 20:21:46 +0300

    indoxtext swift initial

---
+++ froggo.unity/indoxtext.swift/indoxtext.swift/indoxtext_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  indoxtext_swiftApp.swift
+//  indoxtext.swift
+//
+//  Created by Oleksandr Koreniuk on 20.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct indoxtext_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
